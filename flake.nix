61728c117de493ad20f0f9e3d905038bdedbf677
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-02 12:31:55 +0530

    my initial setup
    - minimal setup (gnome with bloat)
    - vscode and cursor for python development

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "my nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=release-24.11";
+  };
+
+  outputs = { self, nixpkgs }: 
+  
+  let
+    system = "x86_64-linux";
+    # legacypackages not like it's old or something
+    # but similar to searching packages in vast amount of packages
+    # you can use `nixpkgs.hello` but it will take a long time to process
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations."unknown" = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [ 
+        ./configuration.nix
+      ];
+    };
+
+  };
+}
