16c3a18e559d7f1832d526e3de250dbabc8a56d7
prajwalBrigosha <prajwal.m@brigosha.com>
2026-04-01 00:07:48 +0530

    fix: nix gc

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,8 +1,12 @@
 { ... }:
 {
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    gc = { keepDays = 30; };
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    gc = { 
+      automatic = true;
+      dates = "weekly"; # or "daily"
+      options = "--delete-older-than 30d";
+    };
   };
   nixpkgs.config.allowUnfree = true;
 }
